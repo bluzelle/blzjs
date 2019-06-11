925599ae73e5643db58d107ad8eb13501610c01e
John Lam <johnlamyc@gmail.com>
2019-06-10 17:26:24 -0700

    Ignore electron in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const WebpackShellPlugin = require('webpack-shell-plugin');
+const webpack = require('webpack');
 
 
-// We want to build for production (i.e. minimize 
+// We want to build for production (i.e. minimize
 // the bluzelle-js code and have no sourcemaps) when
 // deploying.
 
-const mode = 
+const mode =
 
     'TRAVIS_BUILD_STAGE_NAME' in process.env &&
-    process.env['TRAVIS_BUILD_STAGE_NAME'] === 'Deploy' 
-        
+    process.env['TRAVIS_BUILD_STAGE_NAME'] === 'Deploy'
+
         ? 'production' : 'development';
 
 
@@ -26,7 +27,8 @@ module.exports = [
         target: 'node',
 
         plugins: [
-            new WebpackShellPlugin({onBuildStart: ['proto/updateProto.sh']})
+            new WebpackShellPlugin({onBuildStart: ['proto/updateProto.sh']}),
+            new webpack.IgnorePlugin(/^electron$/)
         ],
 
         stats: {
@@ -49,8 +51,8 @@ module.exports = [
             new WebpackShellPlugin({onBuildStart: ['proto/updateProto.sh', 'scripts/scrypt.rb']}),
         ],
 
-        module: { 
-            rules: [   
+        module: {
+            rules: [
                 {
                     test: /\.node$/,
                     use: "node-loader"
