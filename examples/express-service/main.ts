b790bce72e3429dc5cf6a9271b9a6b90dae18b00
scott <scott@bulldoginfo.com>
2020-07-14 19:54:42 -0700

    updated express-service example

--- examples/express-service/main.ts
+++ examples/express-service/main.ts
@@ -1,5 +1,4 @@
 import {bluzelle, API} from 'bluzelle'
-import {memoize} from 'lodash'
 import express from 'express'
 import {bluzelleConfig} from "../example-config";
 
@@ -7,18 +6,16 @@ const PORT = 3000;
 
 const app = express()
 
-const getBZ: () => Promise<API> = memoize(() => bluzelle(bluzelleConfig));
+const bz: API = bluzelle(bluzelleConfig);
 
 app.get('/create/:key/:value', (req, res) => {
-    getBZ()
-        .then(bz => bz.create(req.params.key, req.params.value, {gas_price: 10}))
+        bz.create(req.params.key, req.params.value, {gas_price: 10})
         .then(() => res.send(`Created ${req.params.key}=${req.params.value}`))
         .catch(e => res.send(e.message))
 });
 
 app.get('/read/:key', (req, res) => {
-    getBZ()
-        .then(bz => bz.read(req.params.key))
+        bz.read(req.params.key)
         .then(value => res.send(`value = ${value}`))
         .catch(e => res.send(e.message))
 })
