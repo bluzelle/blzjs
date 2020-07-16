c05e6a06100cf7a38d518573252b11b08ec4ef0c
scott <scott@bulldoginfo.com>
2020-07-16 04:25:09 -0700

    add --transaction-only and --quiet flags to performance demo

--- examples/performance/main.ts
+++ examples/performance/main.ts
@@ -7,6 +7,24 @@ const {bluzelleConfig} = require('../example-config.js')
 
 const GAS_INFO: GasInfo = {gas_price: 10};
 
+const bz = bluzelle(bluzelleConfig)
+
+const program = require('commander');
+
+let numberOfKeys: number = 1;
+let valueLength: number = 1;
+
+program
+    .arguments('[numOfKeys] [valLength]')
+    .description('performance demo')
+    .option('-t, --transaction-only', 'Only run transactional messages')
+    .option('-q, --quiet', 'Do not print each transaction individually')
+    .action((numOfKeys: string, valLength: string) => {
+        numOfKeys && (numberOfKeys = parseInt(numOfKeys));
+        valLength && (valueLength = parseInt(valLength))
+    }).parse(process.argv);
+
+
 
 const withTime = (label: string, fn: () => Promise<any>): Promise<void> => {
     console.log('start:', label)
@@ -15,9 +33,6 @@ const withTime = (label: string, fn: () => Promise<any>): Promise<void> => {
         .then(() => console.log('end:  ', label, Date.now() - start, '\n\n'))
 }
 
-const bz = bluzelle(bluzelleConfig)
-const numberOfKeys = parseInt(process.argv[2] || '1');
-const valueLength = parseInt(process.argv[3] || '1');
 
 
 const error = (e: any) => console.log('ERROR:', e);
@@ -26,7 +41,7 @@ const createWithTransaction = () => withTime(`transactional create ${numberOfKey
     bz.withTransaction(() => Promise.all(
         times(numberOfKeys, (n) =>
             bz.create(`key-transactional-${n}`, '#'.repeat(valueLength), GAS_INFO)
-                .then(res => console.log(`key-${n} created  (${res.txhash})`))
+                .then(res => program.quiet || console.log(`key-${n} created  (${res.txhash})`))
                 .catch(error)
         )
     ))
@@ -36,7 +51,7 @@ const createWithoutTransaction = () => withTime(`non-transactional create ${numb
     Promise.all(
         times(numberOfKeys, (n) =>
             bz.create(`key-non-transactional-${n}`, '#'.repeat(valueLength), GAS_INFO)
-                .then(res => console.log(`key-${n} created (${res.txhash})`))
+                .then(res => program.quiet || console.log(`key-${n} created (${res.txhash})`))
                 .catch(error)
         )
     )
@@ -46,7 +61,7 @@ const txReadWithoutTransaction = () => withTime(`read ${numberOfKeys} keys witho
     Promise.all(
         times(numberOfKeys, (n) =>
             bz.txRead(`key-non-transactional-${n}`, GAS_INFO)
-                .then(res => console.log(`key-${n} read`))
+                .then(res => program.quiet || console.log(`key-${n} read`))
                 .catch(error)
         )
     )
@@ -56,7 +71,7 @@ const txReadWithTransaction = () => withTime(`read ${numberOfKeys} keys with tra
     bz.withTransaction(() => Promise.all(
         times(numberOfKeys, (n) =>
             bz.txRead(`key-transactional-${n}`, GAS_INFO)
-                .then(res => console.log(`key-${n} read`))
+                .then(res => program.quiet || console.log(`key-${n} read`))
                 .catch(error)
         )
     ))
@@ -64,7 +79,7 @@ const txReadWithTransaction = () => withTime(`read ${numberOfKeys} keys with tra
 
 
 Promise.resolve()
-//    .then(createWithoutTransaction)
+    .then(() => program.transactionOnly ? undefined : createWithoutTransaction())
     .then(createWithTransaction)
-//    .then(txReadWithoutTransaction)
+    .then(() => program.transactionOnly ? undefined : txReadWithoutTransaction())
     .then(txReadWithTransaction)
