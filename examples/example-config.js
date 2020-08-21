42e97783e73a9ad3232f3025e695a9ac953a2eab
scott <scott@bulldoginfo.com>
2020-08-21 00:25:40 -0700

    updated example-config

--- examples/example-config.js
+++ examples/example-config.js
@@ -1,6 +1,6 @@
 module.exports.bluzelleConfig = {
     mnemonic: "dish film auto bundle nest hospital arctic giraffe surface afford tribe toe swing flavor outdoor hand slice diesel awesome excess liar impulse trumpet rare",
     chain_id: 'bluzelleTestNetPublic-6',
-    uuid: 'feeder-test',
+    uuid: Date.now().toString(),
     endpoint: 'https://client.sentry.testnet.public.bluzelle.com:1319'
 };
