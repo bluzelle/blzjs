d564ef5aaccb8831ae7ebe157c3130e6c58cd9ff
scott <scott@bulldoginfo.com>
2021-04-01 18:23:52 -0700

    added downloading google proto definitions to compile-proto.sh

--- stargate/compile-proto.sh
+++ stargate/compile-proto.sh
@@ -6,3 +6,7 @@ protoc \
   "./proto/crud/CrudValue.proto" \
   "./proto/crud/tx.proto" \
   "./proto/crud/query.proto"
+
+mkdir -p proto/google/api
+curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto > proto/google/api/annotations.proto
+curl https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto > proto/google/api/http.proto
