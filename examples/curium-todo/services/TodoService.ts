08dc2e80628eb208fcd52382956759cbdadb7dc0
matteyu <matt.r.ilagan@gmail.com>
2021-06-18 16:47:04 -0700

    Update bluzelle lib to stargate sdk-js

--- examples/curium-todo/services/TodoService.ts
+++ examples/curium-todo/services/TodoService.ts
@@ -1,9 +1,10 @@
-import {bluzelle, API, BluzelleConfig} from "bluzelle";
+import {bluzelle} from "@bluzelle/sdk-js";
 import {extend, memoize} from 'lodash'
 import {StoredTodo, Todo} from "../model/Todo";
-import {bluzelleConfig} from "../../example-config";
+import {bluzelleConfig, userConfig} from "../../example-config";
 
-export const params: BluzelleConfig = bluzelleConfig;
+export const params:any = bluzelleConfig;
+export const userParams:any = userConfig;
 
 const todosListeners = [];
 const todos: Record<string, Todo> = {};
@@ -22,25 +23,37 @@ export const storeTodo = (todo: Pick<Todo, 'body'>): Promise<any> => {
     todos[time] = {...todo, time, synced: false, done: false};
     notifyListeners();
     const storedTodo: StoredTodo = {...todo, time, done: false};
-    return bz.create(time, JSON.stringify(storedTodo), {gas_price: 10})
+    return bz
+        .then(client => client.db.tx.Create({	
+            creator: client.db.address,     			// the creator of the transaction should always be the sender's address*
+            uuid: userConfig.uuid,	
+            key: time,
+            value: new TextEncoder().encode(JSON.stringify(storedTodo)),	// values are stored as byte arrays 
+            metadata: new Uint8Array(),			
+            lease: {days: 0, seconds: 0, years: 0, hours: 1, minutes: 0} // Lease object to specify lifespan of key-value**
+       }))
         .then(() => todos[time].synced = true)
         .then(notifyListeners)
 }
-
 
 const notifyListeners = () =>
  todosListeners.forEach(listener => listener(Object.values(todos)));
 
 const loadTodos = (): Promise<void> =>
-        bz.keyValues()
-        .then(result => result.map(it => ({...JSON.parse(it.value), synced: true})))
+        bz
+        .then(client => client.db.q.KeyValues({uuid:client.db.address}))
+        .then(result => result.keyValues.map(it => ({...JSON.parse(new TextDecoder().decode(it.value)), synced: true})))
         .then(list => list.map(it => todos[it.time] = extend(todos[it.time] || {},  it)))
         .then(notifyListeners)
 
 export const deleteTodo = (todo: Todo):Promise<any> => {
     todo.synced = false;
     notifyListeners();
-    return bz.delete(todo.time, {gas_price: 10})
+    return bz
+        .then(client => client.db.tx.DeleteAll({
+            creator: client.db.address,
+            uuid: userConfig.uuid
+        }))
         .then(() => delete todos[todo.time])
         .then(notifyListeners);
 }
@@ -54,7 +67,15 @@ export const toggleTodoDone = (todo: Todo): Promise<any> => {
         body: todo.body,
         time: todo.time
     }
-    return bz.update(todo.time, JSON.stringify(storedTodo), {gas_price: 10})
+    return bz
+        .then(client => client.db.tx.Upsert({
+            creator: client.db.address,
+            uuid: userConfig.uuid,
+            key: todo.time,
+            value: new TextEncoder().encode(JSON.stringify(storedTodo)),
+            metadata: new Uint8Array(),
+            lease: {days: 0, seconds: 0, years: 0, hours: 1, minutes: 0} 
+        }))
         .then(() => todo.synced = true)
         .then(notifyListeners)
 }
