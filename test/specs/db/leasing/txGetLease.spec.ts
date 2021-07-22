2dc731d729e4c6a5e3e3e9a38167e968bfd6adef
Devon Sangha <devonasangha@gmail.com>
2021-07-22 11:23:04 -0700

    txGetLease tests refactored and passing

---
+++ test/specs/db/leasing/txGetLease.spec.ts
@@ -0,0 +1,25 @@
+import {APIAndSwarm, DEFAULT_TIMEOUT, defaultGasParams, sentryWithClient} from "../../../helpers/client-helpers";
+import {useChaiAsPromised} from "../../../helpers/global-helpers";
+import {expect} from 'chai';
+import delay from "delay";
+
+
+describe('txGetLease', function () {
+    this.timeout(DEFAULT_TIMEOUT);
+    let bz: APIAndSwarm;
+
+    beforeEach(() => useChaiAsPromised());
+    beforeEach(() => sentryWithClient()
+        .then(db => bz = db));
+
+    it('should throw exception if key does not exist', () => {
+        return () => expect(bz.txGetLease('fake', defaultGasParams())).to.be.rejectedWith('Key does not exist');
+    });
+
+    it('should return the lease time left', async () => {
+        return bz.create('key', 'value', defaultGasParams(), {seconds: 30})
+            .then(() => delay(20000))
+            .then(() => bz.txGetLease('key', defaultGasParams()))
+            .then(tx => expect(tx.lease).to.be.lessThan(12));
+    });
+});
