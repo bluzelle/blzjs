cc6c1890a21ca69a976fb9bad0b1157a40a5971d
Devon Sangha <devonasangha@gmail.com>
2021-07-22 14:46:49 -0700

    refactored getNShortestLeases, all account() tests passing

--- test/specs/db/leasing/getNShortestLeases.spec.ts
+++ test/specs/db/leasing/getNShortestLeases.spec.ts
@@ -9,19 +9,17 @@ describe('getNShortestLeases()', function () {
     beforeEach(() => sentryWithClient()
         .then(db => bz = db));
 
-    it('should return the first 2 shortest leases', async () => {
-        await Promise.all([
+    it('should return the first 2 shortest leases', () => {
+        return Promise.all([
             bz.create('key1', 'value', defaultGasParams(), {days: 1}),
             bz.create('key2', 'value', defaultGasParams(), {hours: 1}),
             bz.create('key4', 'value', defaultGasParams(), {seconds: 30}),
             bz.create('key3', 'value', defaultGasParams(), {minutes: 1})
-        ]);
-        // .then(() => bz.getNShortestLeases(2))
-        // .then(result => expect(result.find(({key}) => key === 'key3')?.lease).to.be.closeTo(60, 12));
-        // .then(result => expect(result.find(({key}) => key === 'key4')?.lease).to.be.closeTo(30, 12));
-        const result = await bz.getNShortestLeases(2);
-
-        expect(result.find(({key}) => key === 'key3')?.lease).to.be.closeTo(60, 12);
-        expect(result.find(({key}) => key === 'key4')?.lease).to.be.closeTo(30, 12);
+        ])
+            .then(() => bz.getNShortestLeases(2))
+            .then(result => {
+                expect(result.find(({key}) => key === 'key3')?.lease).to.be.closeTo(60, 12);
+                expect(result.find(({key}) => key === 'key4')?.lease).to.be.closeTo(30, 12);
+            });
     });
 });
