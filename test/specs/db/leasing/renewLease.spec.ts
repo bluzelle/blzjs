e427bdf5da87a4bb1119c05aa288ecae5dfe0350
Devon Sangha <devonasangha@gmail.com>
2021-07-22 09:35:18 -0700

    renewLease tests refactored and passing

--- test/specs/db/leasing/renewLease.spec.ts
+++ test/specs/db/leasing/renewLease.spec.ts
@@ -10,13 +10,20 @@ describe('renewLease', function(){
         .then(db => bz =db));
 
     it('should increase the lease time in days',() => {
-        return bz.create('myKey', 'myValue', defaultGasParams(), {days: 1})
-            .then(() => bz.getLease('myKey'))
+        return bz.create('key', 'value', defaultGasParams(), {days: 1})
+            .then(() => bz.getLease('key'))
             .then(lease => expect(lease).to.be.closeTo(86400, 12))
-            .then(() => bz.renewLease('myKey', defaultGasParams(), {days: 2}))
-            .then(() => bz.getLease('myKey'))
+            .then(() => bz.renewLease('key', defaultGasParams(), {days: 2}))
+            .then(() => bz.getLease('key'))
             .then(lease => expect(lease).to.be.closeTo(172800, 12));
     });
 
-
+    it('should reduce the lease time',() => {
+        return bz.create('key', 'value', defaultGasParams(), {seconds: 10000})
+            .then(() => bz.getLease('key'))
+            .then(lease => expect(lease).to.be.closeTo(10000, 12))
+            .then(() => bz.renewLease('key', defaultGasParams(), {seconds: 100}))
+            .then(() => bz.getLease('key'))
+            .then(lease => expect(lease).to.be.closeTo(100, 12));
+    });
 });
