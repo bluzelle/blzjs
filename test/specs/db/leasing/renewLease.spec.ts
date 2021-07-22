2881563dea3c322b33c320f22abc07427089320b
Devon Sangha <devonasangha@gmail.com>
2021-07-22 09:17:27 -0700

    Refactored renewLease test for days parameter

--- test/specs/db/leasing/renewLease.spec.ts
+++ test/specs/db/leasing/renewLease.spec.ts
@@ -2,17 +2,21 @@ import {APIAndSwarm, DEFAULT_TIMEOUT, defaultGasParams, sentryWithClient} from "
 import {expect} from 'chai';
 
 
-describe('renewLease', function() {
+describe('renewLease', function(){
     this.timeout(DEFAULT_TIMEOUT);
     let bz: APIAndSwarm;
 
     beforeEach(() => sentryWithClient()
         .then(db => bz =db));
 
-    it('should increase the lease time in days', async () => {
-        await bz.create('myKey', 'myValue', defaultGasParams(), {days: 1});
-        expect(await bz.getLease('myKey')).to.be.closeTo(86400, 12);
-        await bz.renewLease('myKey', defaultGasParams(), {days: 2});
-        expect(await bz.getLease('myKey')).to.be.closeTo(172800, 12);
+    it('should increase the lease time in days',() => {
+        return bz.create('myKey', 'myValue', defaultGasParams(), {days: 1})
+            .then(() => bz.getLease('myKey'))
+            .then(lease => expect(lease).to.be.closeTo(86400, 12))
+            .then(() => bz.renewLease('myKey', defaultGasParams(), {days: 2}))
+            .then(() => bz.getLease('myKey'))
+            .then(lease => expect(lease).to.be.closeTo(172800, 12));
     });
+
+
 });
