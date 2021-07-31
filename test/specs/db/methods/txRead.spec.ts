fb3a1c866e5e7a4eee378a376a38e6500b3c2d67
Devon Sangha <devonasangha@gmail.com>
2021-07-30 17:18:02 -0700

    txRead() tests refactored and passing, will ask Scott about handle parallel reads test

---
+++ test/specs/db/methods/txRead.spec.ts
@@ -0,0 +1,67 @@
+import {
+    APIAndSwarm,
+    createKeys,
+    DEFAULT_TIMEOUT,
+    defaultGasParams,
+    sentryWithClient
+} from "../../../helpers/client-helpers";
+import {useChaiAsPromised} from "../../../helpers/global-helpers";
+import {expect} from "chai";
+
+describe('txread()', function () {
+    this.timeout(DEFAULT_TIMEOUT);
+    let bz: APIAndSwarm;
+
+    beforeEach(() => sentryWithClient()
+        .then(db => bz = db)
+        .then(() => useChaiAsPromised())
+    );
+
+    it('should work with empty value', () => {
+        return bz.create('key', '', defaultGasParams())
+            .then(() => bz.txRead('key', defaultGasParams()))
+            .then(tx => expect(tx).to.have.property('value', ''));
+    });
+
+    it('should retrieve values in order', () => {
+        return Promise.all([
+            bz.create('key', 'value', defaultGasParams()),
+            bz.txRead('key', defaultGasParams()),
+            bz.update('key', 'newValue', defaultGasParams()),
+            bz.txRead('key', defaultGasParams())
+        ])
+            .then(results => {
+                expect(results[1]?.value).to.equal('value');
+                expect(results[3]?.value).to.equal('newValue');
+            });
+    });
+
+    it('should retrieve a value from the store', () => {
+        return bz.create('key', 'value', defaultGasParams())
+            .then(() => bz.txRead('key', defaultGasParams()))
+            .then(tx => expect(tx?.value).to.equal('value'));
+    });
+
+    it('should throw an error if key does not exist', () => {
+        return bz.txRead('key', defaultGasParams())
+            .catch(e => expect(e.error).to.equal('invalid request: Key does not exist: failed to execute message'));
+    });
+
+    //Ask Scott about this test
+    it('should handle parallel reads', () => {
+        return createKeys(bz, 3)
+            .then(() => Promise.all([
+                    bz.txRead('key0', defaultGasParams())
+                        .then(tx => expect(tx?.value).to.equal('value')),
+                    bz.txRead('key1', defaultGasParams())
+                        .then(tx => expect(tx?.value).to.equal('value')),
+                    bz.txRead('key2', defaultGasParams())
+                        .then(tx => expect(tx?.value).to.equal('value'))
+                ])
+            );
+        // const {keys, values} = await createKeys(bz, 5);
+        // expect(
+        //     await Promise.all(keys.map(key => bz.txRead(key, defaultGasParams()).then(x => x?.value)))
+        // ).to.deep.equal(values);
+    });
+});
